2e61ee49ae99dfc6ca2ed31820b0ae05670cf98a
SET001 <settydark@gmail.com>
2018-07-19 16:21:47 +0300

    case with no token

--- middlewares/checkJWT.js
+++ middlewares/checkJWT.js
@@ -4,14 +4,15 @@ const config = require('../config')
 module.exports = (req, res, next)=> {
 	if (!req.headers.authorization){
 		res.status(401).send('Missing auth token')
+	} else {
+		const token = req.headers.authorization.split('=').pop()
+		jwt.verify(token, config.jwtSecret, (err, decoded) => {
+			if (err){
+				res.status(401).send(err.message)
+			} else {
+				req.user = decoded
+				next()
+			}
+		});
 	}
-	const token = req.headers.authorization.split('=').pop()
-	jwt.verify(token, config.jwtSecret, (err, decoded) => {
-		if (err){
-			res.status(401).send(err.message)
-		} else {
-			req.user = decoded
-			next()
-		}
-	});
 }
